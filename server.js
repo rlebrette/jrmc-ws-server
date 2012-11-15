b45af02b9fa398f62ea16a95937b45a30963f313
Regis Le Brettevillois <regis@lebrette.net>
2012-11-15 13:04:18 +0100

    Configuration is externalized.

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 var rjs = require('requirejs');
-rjs(['./jrmc-ws-server', './jrmc-ws-server-util'],
-    function (JRMC, util) {
-        var configuration = {
-            port:1337,
-            webPort:8080,
-            jrmcHost:'localhost',
-            jrmcPort:52199,
-//            jrmcAuthenticate:'rlebrette:1',
-            logger:{info:util.std, trace:util.std}
-        }
+rjs(['./jrmc-ws-server', './jrmc-ws-server-util' , 'fs'],
+    function (JRMC, util, fs) {
+        fs.readFile('configuration.json', function (err, data) {
+            if (err) throw err;
+            var configuration = JSON.parse(data);
+            configuration.logger = {info: util.std, trace: util.std}
+            // Starts the server
+            new JRMC.Server(configuration);
+        });
 
-        // Starts the server
-        new JRMC.Server(configuration);
+
     });
 
